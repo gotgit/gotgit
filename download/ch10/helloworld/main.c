b45d6a566af9a9c9fe2d38db22b40336c788e4b7
Jiang Xin <jiangxin@ossxp.com>
2011-05-30 19:28:27 +0800

    Hello world example.

---
+++ download/ch10/helloworld/main.c
@@ -0,0 +1,10 @@
+#include "version.h"
+#include <stdio.h>
+
+int
+main()
+{
+    printf( "Hello, world.\n" );
+    printf( "version: %s.\n", _VERSION );
+    return 0;
+}
---
+++ download/ch10/helloworld/version.h.in
@@ -0,0 +1,6 @@
+#ifndef HELLO_WORLD_VERSION_H
+#define HELLO_WORLD_VERSION_H
+
+#define _VERSION "<version>"
+
+#endif
